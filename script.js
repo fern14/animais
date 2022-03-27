7f56f0417187c20f6bbfdc10ed81ecb23a64463e
David Fernandes <85141697+fern14@users.noreply.github.com>
2022-03-27 18:40:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,153 @@
+// import { initFechAnimais } from "./fetch-animais";
+
+function initAcoordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const ativo = "ativo";
+  if (accordionList.length) {
+    accordionList[0].classList.add(ativo);
+    accordionList[0].nextElementSibling.classList.add(ativo);
+
+    function activeAccordion() {
+      this.classList.toggle(ativo);
+      this.nextElementSibling.classList.toggle(ativo);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAcoordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    // preventDefault - previne o evento padrão
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
+
+const target = document.querySelectorAll("[data-anime]");
+const animationClass = "animate";
+
+function animeScroll() {
+  const windowTop = window.scrollY + (window.innerHeight * 3) / 4;
+  target.forEach((e) => {
+    if (windowTop > e.offsetTop) {
+      e.classList.add(animationClass);
+    } else {
+      e.classList.remove(animationClass);
+    }
+    e.offsetTop;
+  });
+}
+
+animeScroll();
+
+if (target.length) {
+  window.addEventListener("scroll", () => {
+    animeScroll();
+  });
+}
+
+const tabMenu = document.querySelectorAll(".js-tabmenu li");
+const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+if (tabMenu.length && tabContent.length) {
+  tabContent[0].classList.add("ativo");
+
+  function activeTab(index) {
+    tabContent.forEach((section) => {
+      section.classList.remove("ativo");
+    });
+    tabContent[index].classList.add("ativo");
+  }
+
+  tabMenu.forEach((item, index) => {
+    item.addEventListener("click", () => {
+      activeTab(index);
+    });
+  });
+}
+
+// modal
+function initModal() {
+  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
+  const botaoFechar = document.querySelector('[data-modal="fechar"]');
+  const containerModal = document.querySelector('[data-modal="container"]');
+
+  if (botaoAbrir && botaoFechar && containerModal) {
+    function toggleModal(event) {
+      event.preventDefault();
+      containerModal.classList.toggle("ativo");
+    }
+
+    function cliqueForaModal(event) {
+      if (event.target === this) {
+        toggleModal(event);
+      }
+    }
+
+    botaoAbrir.addEventListener("click", toggleModal);
+    botaoFechar.addEventListener("click", toggleModal);
+    containerModal.addEventListener("click", cliqueForaModal);
+  }
+}
+initModal();
+
+// dropdown menu
+
+const dropdownMenus = document.querySelectorAll("[data-dropdown]");
+
+dropdownMenus.forEach((menu) => {
+  ["touchstart", "click"].forEach((userEvent) => {
+    menu.addEventListener(userEvent, handleClick);
+  });
+});
+
+function handleClick(event) {
+  event.preventDefault();
+  this.classList.add("active");
+  outsideClick(this, ["touchstart", "click"], () => {
+    this.classList.remove("active");
+  });
+}
+
+// outside click
+
+function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = "data-outside";
+
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      html.addEventListener(userEvent, handleOutsideClick);
+    });
+    element.setAttribute(outside);
+  }
+
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      events.forEach((userEvent) => {
+        html.removeEventListener(userEvent, handleOutsideClick);
+      });
+      callback();
+    }
+  }
+}
